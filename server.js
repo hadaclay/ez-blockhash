ebebb4ba794fda1677c5295ea393a8ef843bd1c1
Clayton Hadaway <clayton@codefor.cash>
2019-06-10 00:14:34 -0500

    change port to process.env port

--- server.js
+++ server.js
@@ -43,4 +43,4 @@ app.post('/upload', upload.single('image-upload'), (req, res, next) => {
   });
 });
 
-app.listen(3000, () => console.log('Server listening on port 3000'));
+app.listen(process.env.PORT, () => console.log(`Server listening on port ${process.env.PORT}`));
