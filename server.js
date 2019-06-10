04a5a4dca3a519a6587328adc0acd7599ce2d536
Clayton Hadaway <clayton@codefor.cash>
2019-06-10 00:10:47 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const { imageHash } = require('image-hash');
+
+const app = express();
+
+const storage = multer.diskStorage({
+  dest: 'uploads/',
+  filename: (req, file, cb) => {
+    cb(null, path.basename(file.originalname) + '-' + Date.now() + path.extname(file.originalname));
+  } 
+});
+
+const upload = multer({storage});
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/upload', upload.single('image-upload'), (req, res, next) => {
+  const file = req.file;
+  if (!file) {
+    const error = new Error('No File Uploaded');
+    error.httpStatusCode = 400;
+    return next(error);
+  }
+
+  const precise = !!req.body.precise;
+  const bits = Number(req.body.bitAmount);
+
+  imageHash(req.file.path, bits, precise, (error, hash) => {
+    if (error) {
+      return res.status(400).send('Error!');
+    }
+
+    res.set('Content-Type', 'text/html');
+
+    res.send(`<html><head><title>EZ Block-Hash</title></head><body>Hash: ${hash}</body></html>`);
+  });
+});
+
+app.listen(3000, () => console.log('Server listening on port 3000'));
